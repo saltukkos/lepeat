51629cc59e6a77c8094121f7e1f62a4672f7d04a
Anastasia Saltuk <saltukanas@gmail.com>
2023-12-18 22:47:57 +0100

    Add redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import WordCheckComponent from './training/WordCheckComponent';
+import { store } from "./redux/store"
+import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+
+ReactDOM.render(
+    //This makes the store accessible to the App that is passing it as a prop
+  <Provider store={store}>
     <WordCheckComponent />
-  </React.StrictMode>
-);
+  </Provider>,
+document.getElementById('root')
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <WordCheckComponent />
+//   </React.StrictMode>
+// );
+

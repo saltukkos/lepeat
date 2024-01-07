bae8d11239b7c10fa2096eedd2d4648cf1feb3a9
saltuk <saltukkos@gmail.com>
2024-01-07 16:16:34 +0100

    Add toast for profile import/export

--- frontend/src/components/dataExchange/ProfileImportExport.tsx
+++ frontend/src/components/dataExchange/ProfileImportExport.tsx
@@ -1,14 +1,18 @@
-import React, {CSSProperties, useState} from 'react';
-import { Container } from 'react-bootstrap';
+import React, {CSSProperties, useRef, useState} from 'react';
+import {Container} from 'react-bootstrap';
 import {LepeatProfile} from "../../model/LepeatProfile";
 import {deserializeProfile, serializeProfile} from "../../services/ProfileSerializer";
 import {useDispatch, useSelector} from "react-redux";
+import {CToast, CToastBody, CToaster,} from '@coreui/react'
 
 function ProfileImportExport() {
     const profile = useSelector<any>((state) => state.profile) as LepeatProfile; //TODO save types
     const dispatch = useDispatch()
 
     const [textBoxData, setTextBoxData] = useState(serializeProfile(profile));
+
+    const [toast, addToast] = useState(0)
+    const toaster = useRef<HTMLDivElement>(null!)
 
     const copyToClipboard = () => {
         navigator.clipboard.writeText(textBoxData);
@@ -19,8 +23,20 @@ function ProfileImportExport() {
         let newProfile = deserializeProfile(textBoxData);
         if (newProfile) {
             dispatch({ type: 'set', profile: newProfile });
+
+            // @ts-expect-error This shit does not work
+            addToast((
+                <CToast autohide={true} delay={2000}>
+                    <CToastBody>Profile is loaded</CToastBody>
+                </CToast>
+            ))
         } else {
-            alert("Can't deserialize profile")
+            // @ts-expect-error This shit does not work
+            addToast((
+                <CToast autohide={true} delay={2000} color={"danger"}>
+                    <CToastBody>Can't deserialize profile</CToastBody>
+                </CToast>
+            ))
         }
     };
 
@@ -38,11 +54,16 @@ function ProfileImportExport() {
         height: '90vh',
     };
 
+    
     return (
         <Container className="page d-flex flex-column align-items-center" style={containerStyle}>
             <textarea style={textAreaStyle} value={textBoxData} onChange={handleInput}/>
             <button onClick={copyToClipboard}>Copy textbox data to clipboard</button>
             <button onClick={setProfileFromTextBoxData}>Set profile from textbox data</button>
+            <CToaster ref={toaster}
+                // @ts-expect-error This shit does not work
+                      push={toast}
+                      placement="top-end"/>
         </Container>
     );
 }

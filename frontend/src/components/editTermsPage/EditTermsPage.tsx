e71d670ee755a5d3f6a21e46cd3df696fece30c8
Anastasia Saltuk <saltukanas@gmail.com>
2024-01-11 21:29:06 +0100

    Improve edit terms page

--- frontend/src/components/editTermsPage/EditTermsPage.tsx
+++ frontend/src/components/editTermsPage/EditTermsPage.tsx
@@ -2,27 +2,31 @@ import React, {useContext, useState} from "react";
 import {AttributeDefinition} from "../../model/AttributeDefinition";
 import ProfileContext from "../../contexts/ProfileContext";
 import {markProfileDirty} from "../../services/Persistence";
+import {CButton, CFormInput, CFormLabel, CTable, CTableBody, CTableHeaderCell, CTableRow} from "@coreui/react";
 
 function EditTermsPage() {
     const { profile } = useContext(ProfileContext);
 
-    const terms = profile.terms;
-
     const [editableTermIdx, setEditableTermIdx] = useState(-1);
     const [inputsData, setInputsData] = useState<[AttributeDefinition, string][]>([]);
+    const [termsToRender, setTermsToRender] = useState(profile.terms);
 
     const editTerm = (idx: number) => {
-        setInputsData(Array.from(terms[idx].attributeValues.entries()));
+        setInputsData(Array.from(termsToRender[idx].attributeValues.entries()));
+        console.log(idx);
         setEditableTermIdx(idx);
     }
 
-    //TODO implement
-    const removeTerm = (idx: number) => {}
+    const removeTerm = (idx: number) => {
+        profile.terms.splice(idx, 1);
+        setTermsToRender([...profile.terms]);
+        markProfileDirty(profile);
+    }
 
     const onChangeInput = (attribute: AttributeDefinition, newValue: string, idx: number) => {
-        setInputsData(id => {
-            id[idx] = [attribute, newValue];
-            return [...id];
+        setInputsData(inputs => {
+            inputs[idx] = [attribute, newValue];
+            return [...inputs];
         })
     }
 
@@ -32,36 +36,41 @@ function EditTermsPage() {
             newTermAttributesData.set(inputsData[i][0], inputsData[i][1]);
         }
 
-        terms[editableTermIdx].attributeValues = newTermAttributesData
+        termsToRender[editableTermIdx].attributeValues = newTermAttributesData
         markProfileDirty(profile);
         setEditableTermIdx(-1);
     }
 
-
     return (
-        <div>
-            {terms.map((t, idx) => {
-                const termRow = idx === editableTermIdx ?
-                    <>
-                        {
-                            inputsData.map((attributeData, attrIdx) => {
-                                return (
-                                    <input key={`${attributeData[1]}-${attrIdx}`} type="text" id="idx" value={attributeData[1]}
-                                           onChange={(e) => onChangeInput(attributeData[0], e.target.value, attrIdx)}/>
-                            )
-                            })
-                        }
-                    </> : Array.from(t.attributeValues.values()).join("; ");
+        <CTable>
+            <CTableBody>
+                {termsToRender.map((t, idx) => {
+                    const termRow = idx === editableTermIdx ?
+                        <>
+                            {
+                                inputsData.map((attributeData, attrIdx) => {
+                                    return (
+                                        <CFormInput key={`${attrIdx}`} type="text" id="idx" value={attributeData[1]}
+                                               onChange={(e) => onChangeInput(attributeData[0], e.target.value, attrIdx)}/>
+                                    )
+                                })
+                            }
+                        </> : <CFormLabel>{Array.from(t.attributeValues.values()).join("; ")}</CFormLabel> ;
 
-                return (
-                    <div key={`term-${idx}`}>
-                        {termRow}
-                        {idx === editableTermIdx ?<button onClick={() => onSaveEdition()}>Save</button> : <button onClick={() => editTerm(idx)}>Edit</button>}
-                        <button>Remove</button>
-                    </div>
-                )
-            })}
-        </div>
+                    return (
+                        <CTableRow key={`term-${idx}`} color={idx % 2 == 0 ? "light" : ""}>
+                            <CTableHeaderCell className="w-75">{termRow}</CTableHeaderCell>
+                            <CTableHeaderCell>
+                                {idx === editableTermIdx ?<CButton className="mx-2" color={"info"} onClick={() => onSaveEdition()}>Save</CButton> : <CButton className="mx-2" color={"success"} onClick={() => editTerm(idx)}>Edit</CButton>}
+                            </CTableHeaderCell>
+                            <CTableHeaderCell>
+                                <CButton className="mx-1" color={"warning"} onClick={() => removeTerm(idx)}>Remove</CButton>
+                            </CTableHeaderCell>
+                        </CTableRow>
+                    )
+                })}
+            </CTableBody>
+        </CTable>
     )
 }
 

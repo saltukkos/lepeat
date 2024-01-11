0057183a3acd8796e389f33708cf61844a5bf810
saltuk <saltukkos@gmail.com>
2024-01-11 23:17:29 +0100

    Implement different card orders

--- frontend/src/components/training/TrainingSession.tsx
+++ frontend/src/components/training/TrainingSession.tsx
@@ -1,12 +1,22 @@
-import React, {useContext, useState} from "react";
+import React, {ChangeEvent, useContext, useMemo, useState} from "react";
 import {Container} from "react-bootstrap";
 import Card from '../card/Card';
 import './wordCheck.scss';
 import {useLocation, useNavigate} from "react-router-dom";
 import {getTermsToTrain} from "../../services/TrainingStarter";
-import {CButton} from "@coreui/react";
+import {CButton, CFormSelect, CInputGroup, CInputGroupText} from "@coreui/react";
 import ProfileContext from "../../contexts/ProfileContext";
 import {markProfileDirty} from "../../services/Persistence";
+
+type Order = 'dateAdded' | 'dateAddedReverse' | 'lastTrained' | 'lastTrainedReverse' | 'random';
+
+const orderOptions: { label: string; value: Order }[] = [
+    {label: "Date added: oldest first", value: 'dateAdded'},
+    {label: "Date added: newest first", value: 'dateAddedReverse'},
+    {label: "Date last trained: oldest first", value: 'lastTrained'},
+    {label: "Date last trained: latest first", value: 'lastTrainedReverse'},
+    {label: "Shuffled", value: 'random'}
+];
 
 function TrainingSession() {
     const navigate = useNavigate();
@@ -16,8 +26,36 @@ function TrainingSession() {
     const {profile} = useContext(ProfileContext);
     const trainingDefinition = profile.trainingDefinitions.find(value => value.name === trainingName);
 
-    const termTrainingProgress = React.useMemo(() => trainingDefinition ? getTermsToTrain(profile, trainingDefinition, trainingType) : undefined, [profile, trainingDefinition, trainingType]);
     const [currentTermIdx, setCurrentTermIdx] = useState(0);
+    const [orderObject, setOrderObject] = useState<{ order: Order }>({order: 'dateAdded'});
+    // hack: we wrap Order in a new object to force memo reevaluation since we can change the order in the middle 
+    // of a train, and then we don't want to traverse already trained terms  
+    
+    const termTrainingProgress = useMemo(() => {
+        if (trainingDefinition) {
+            const termsToTrain = getTermsToTrain(profile, trainingDefinition, trainingType);
+            const order = orderObject.order;
+            switch(order) {
+                case 'dateAdded':
+                    return termsToTrain;
+                case 'dateAddedReverse':
+                    return termsToTrain.reverse();
+                case 'lastTrained':
+                    return termsToTrain.sort((a, b) =>
+                        (a.lastTrainingDate || Number.MAX_VALUE) - (b.lastTrainingDate || Number.MAX_VALUE)
+                    );
+                case 'lastTrainedReverse':
+                    return termsToTrain.sort((a, b) =>
+                        (b.lastTrainingDate || Number.MIN_VALUE) - (a.lastTrainingDate || Number.MIN_VALUE)
+                    );
+                case 'random':
+                    return termsToTrain.sort(() => Math.random() - 0.5);
+                default:
+                    return termsToTrain;
+            }
+        } 
+        return undefined;
+    }, [profile, trainingDefinition, trainingType, orderObject]);
 
     if (!trainingDefinition || !termTrainingProgress) {
         return ("Unknown training");
@@ -58,8 +96,24 @@ function TrainingSession() {
         setCurrentTermIdx((currentValue) => currentValue + 1);
     }
 
+    const onChangeOrder = (e : ChangeEvent<HTMLSelectElement>) => {
+        setOrderObject({order: e.target.value as Order});
+        setCurrentTermIdx(0);
+    };
+
     return (
         <Container className="page gap-3">
+
+            {/* TODO: save the last selection? */}
+            <CInputGroup className="mb-3 flex-grow-0 w-auto">
+                <CInputGroupText component="label">Order:</CInputGroupText>
+                <CFormSelect
+                    aria-label="Default select example"
+                    onChange={onChangeOrder}
+                    options={orderOptions}
+                />
+            </CInputGroup>
+
             <Card question={question}
                   answer={answer}
                   onRightClicked={onRightClicked}
